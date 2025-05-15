2741aff61bffd6538781efe53ea6e77df36a819d
rishitha3019 <kalvakolrishitha@gmail.com>
2025-05-15 17:33:44 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Rishithareddy-portfolio/', 
+  base: '/Rishithareddy-portfolio/', // 👈 MUST match your repo name
 })
-
